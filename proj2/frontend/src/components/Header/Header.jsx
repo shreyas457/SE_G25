4071a04af2fead5762983dc1955d53e8f274022d
SMRUTHI BT <67542607+smruthibt@users.noreply.github.com>
2025-10-20 11:10:49 -0400

    chore(misc): add/update 24 files
    Includes:
    
    - frontend/
    - frontend/src/Context/
    - frontend/src/assets/
    - frontend/src/assets/bag_icon.png
    - frontend/src/assets/facebook_icon.png
    - frontend/src/assets/food_12.png
    - frontend/src/assets/food_16.png
    - frontend/src/assets/food_2.png
    - frontend/src/assets/food_23.png
    - frontend/src/assets/food_27.png
    - frontend/src/assets/food_30.png
    - frontend/src/assets/food_5.png
    - frontend/src/assets/food_9.png
    - frontend/src/assets/logo.png
    - frontend/src/assets/menu_3.png
    - frontend/src/assets/menu_7.png
    - frontend/src/assets/profile_icon.png
    - frontend/src/assets/selector_icon.png
    - frontend/src/assets/sq_menu_4.png
    - frontend/src/assets/sq_menu_8.png
    - frontend/src/components/AppDownload/
    - frontend/src/components/Footer/
    - frontend/src/pages/MyOrders/

---
+++ proj2/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2> Byte into the future of food</h2>
+                <p>Elevate your dining experience with ByteBite. Discover handcafted meals from top local chefs, delivered seamlessly. Taste the blend of quality, speed, and innovation.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
