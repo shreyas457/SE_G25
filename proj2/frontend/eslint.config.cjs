be96cc38c3534e394f95cff0a7d550388f85d9ae
SRP457 <shreyasraviprasad457@gmail.com>
2025-11-05 20:49:52 -0500

    chore: fix lint file

--- proj2/frontend/eslint.config.cjs
+++ proj2/frontend/eslint.config.cjs
@@ -1,19 +1,20 @@
 // eslint.config.cjs
-const js = require("@eslint/js");
 const react = require("eslint-plugin-react");
 const reactHooks = require("eslint-plugin-react-hooks");
 const reactRefresh = require("eslint-plugin-react-refresh");
+const js = require("@eslint/js");
 
 module.exports = [
   js.configs.recommended,
   react.configs.recommended,
   reactHooks.configs.recommended,
   {
-    files: ["**/*.{js,jsx}"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: { ecmaVersion: "latest", sourceType: "module" },
-    plugins: { reactRefresh },
+    plugins: { "react-refresh": reactRefresh }, // <-- object format
     rules: {
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react/jsx-no-target-blank": "off"
     },
     settings: { react: { version: "18.2" } },
     ignores: ["dist"],
