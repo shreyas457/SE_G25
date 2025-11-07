8324508c0dcc53037230188500198dd2ff84a440
SRP457 <shreyasraviprasad457@gmail.com>
2025-11-06 22:22:39 -0500

    fix: add code coverage badge

--- proj2/admin/eslint.config.cjs
+++ proj2/admin/eslint.config.cjs
@@ -3,6 +3,7 @@ const reactRefresh = require("eslint-plugin-react-refresh");
 
 module.exports = [
   {
+    root: true,
     files: ["**/*.{js,jsx}"],
     ignores: ["dist", ".eslintrc.cjs"],
     languageOptions: { ecmaVersion: "latest", sourceType: "module" },
